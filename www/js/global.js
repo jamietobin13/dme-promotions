c1331ae0f03f760e4c4547c1892af351a829d761
jamietobin13 <42469027+jamietobin13@users.noreply.github.com>
2020-11-08 16:10:25 +0100

    event testing

--- www/js/global.js
+++ www/js/global.js
@@ -13,6 +13,8 @@ var app = {
 
         // Bind any events that are required on startup. Common events are:
         // 'load', 'deviceready', 'offline', and 'online'.
+        document.addEventListener('offline', this.onOffline, false);
+        document.addEventListener('online', this.onOnline, false);
         document.addEventListener('deviceready', this.onDeviceReady, false);
 
     },
@@ -21,7 +23,21 @@ var app = {
         app.receivedEvent('deviceready');
 
     },
+    onOffline: function(){
+
+        app.receivedEvent('offline');
+
+    },
+    onOnline: function(){
+
+        app.receivedEvent('online');
+
+    },
     receivedEvent: function(id) {
+
+        alert( 'id: ' + id );
+        //if( id == 'offline' ){ alert('device offline'); } 
+        //if( id == 'online' ){ alert('device online'); }
 
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
@@ -29,93 +45,11 @@ var app = {
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-        console.log('Received Event: ' + id);
+        
     }
+
 };
 
 
 
-/*
-|--------------------------------------------------------------------------
-| GLOBAL VARS
-|--------------------------------------------------------------------------
-*/
-
-var ajaxUrl = 'https://dev.crystalmountainmedia.net/clients/dme/wp-content/themes/dme-promotions/app.php';
-//var ajaxUrl = 'https://dme-promotions.com/wp-content/themes/dme-promotions/app.php';
-//var paymentUrl = 'https://www.hotcountrytv.com/wp/wp-content/themes/hot-country-tv/app-payment.php';
-//var adminAjaxUrl = 'https://www.hotcountrytv.com/wp/wp-admin/admin-ajax.php';
-
-
-/*
-|--------------------------------------------------------------------------
-| GLOBAL FUNCTIONS
-|--------------------------------------------------------------------------
-*/
-
-function getPageContent(){
-
-    $.ajax({
-          type: "GET",
-          url: ajaxUrl,
-          data: {type: 'banner-adverts'},
-          success: function (result) {
-              $('section.banner-adverts .swiper-wrapper').html( result );
-              initiateSwiper();
-          }
-    });
-
-
-    /*if( $('body').hasClass('page-home')){
-
-       $('.spinner').fadeIn(200);
-       $.ajax({
-              type: "GET",
-              url: ajaxUrl,
-              data: {type: 'page-home', version: $('body').data('version') },
-              success: function (result) {
-                  $('.spinner').fadeOut(200);
-                  $('section.main').append( result );
-                  setVideoHeight();
-              }
-        });
-    }*/
-}
-
-/*
-|--------------------------------------------------------------------------
-| DOCUMENT READY
-|--------------------------------------------------------------------------
-*/
-
-
-$( document ).ready( function(){
-
-
-    /*
-    |--------------------------------------------------------------------------
-    | BACK BUTTON LINKS
-    |--------------------------------------------------------------------------
-    */
-
-    // local storage to keep previous page?
-    /*$('.back-button').on('click', function( e ){
-        e.preventDefault();
-        window.history.back();
-    });*/
-
-    /*
-    |--------------------------------------------------------------------------
-    | LOAD ADVERTS
-    |--------------------------------------------------------------------------
-    */
-
-    //getPageContent();    
-
-
-});
-
-
-//$( window ).load( function(){ setVideoHeight(); });
-//$( window ).resize(function(){ setVideoHeight(); });
 
